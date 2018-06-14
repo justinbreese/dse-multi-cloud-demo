70eca110818499a38c89020062eebc7084cfd888
Justin Breese <jbreese00@hotmail.com>
2018-06-14 07:47:46 -0700

    supressed CLI messages

--- setup.py
+++ setup.py
@@ -38,17 +38,17 @@ repo_user = os.environ.get('academy_user').strip()
 repo_pass = os.environ.get('academy_pass').strip()
 download_token = os.environ.get('academy_token').strip()
 
-#SSH into the OpsCetner/LCM server, install the JDK, install OpsCenter, and then wait 45 seconds for everything to start up
-bashCommand = 'ssh -i '+ ssh_key+ ' '+ username+'@'+server_ip+' \'sudo apt install -y software-properties-common; \
+#SSH into the OpsCenter/LCM server, install the JDK, install OpsCenter, and then wait 45 seconds for everything to start up
+bashCommand = 'ssh -o StrictHostKeyChecking=accept-new -i '+ ssh_key+ ' '+ username+'@'+server_ip+' \'sudo apt-get install -y python software-properties-common; \
 sudo apt-add-repository -y ppa:webupd8team/java; \
-sudo apt update; \
+sudo apt-get update; \
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections; \
-sudo apt install -y oracle-java8-installer; \
+sudo apt-get install -y oracle-java8-installer; \
 echo "deb https://'+repo_user+':'+download_token+'@debian.datastax.com/enterprise \
 stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.sources.list; \
 curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add - ; \
 sudo apt-get update; sudo apt-get install opscenter; sudo service opscenterd start;\
-sleep 45;\''
+sleep 45;\' 2>/dev/null'
 
 output = subprocess.check_output(['bash','-c', bashCommand])
 
